a9da076ff96656d7768e61255799090677f25b66
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-01-23 10:59:32 -0500

    Add Main.py File

---
+++ main.py
@@ -0,0 +1,69 @@
+# Imports
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+# #Settings
+owm_key = input('Enter key: ')
+owm = OWM(owm_key)
+mgr = owm.weather_manager()
+city_input = input('Enter city name: ')
+observation = mgr.weather_at_place(city_input)
+w = observation.weather
+#Funcs
+def menu_print():
+    print('1)Show status.')
+    print('2)Show wind info.')
+    print('3)Show temperature.')
+    print('4)Show cloud percent.')
+    print('5)Show humidity info.')
+
+def show_status():
+    print(w.detailed_status)
+
+def wind_info():
+    print(w.wind())
+
+def teperature(choose):
+    if choose == 'celsius':
+        print(w.temperature('celsius'))
+    elif choose == 'fahrenheit':
+        print(w.temperature('fahrenheit'))
+    else:
+        print("Err!")
+
+def cloud_per():
+    print(w.clouds, '%')
+
+def humidity():
+    print(w.humidity)
+
+#Work
+
+menu_print()
+
+menu_choose = int(input("Choose (1-5): "))
+
+while True:
+    if menu_choose == 1:
+        print("===RESULT===")
+        show_status()
+    elif menu_choose == 2:
+        print("===RESULT===")
+        wind_info()
+    elif menu_choose == 3:
+        print("===RESULT===")
+        temperature()
+    elif menu_choose == 4:
+        print("===RESULT===")
+        cloud_per()
+    elif menu_choose == 5:
+        print("===RESULT===")
+        humidity()
+    con_choose == input("Do you want continue (Yes/No): ")
+    if con_choose == "Yes":
+        pass
+    elif con_choose == "No":
+        print("Bye :3")
+        break
+    else:
+        print("Err!")
